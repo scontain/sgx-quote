b981d4fe337204d7da1d00a651b27d93f06b8c4d
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-10-24 10:15:24 +0200

    refactor(parsers): make all functions parser combinators

--- src/parsers.rs
+++ src/parsers.rs
@@ -14,7 +14,7 @@ pub const REPORT_SIZE: usize = 384;
 pub(crate) fn parse_quote(input: &[u8]) -> IResult<&[u8], Quote> {
     let (i, HeaderExt { header, ak_ty }) = parse_header_ext(input)?;
     let (i, isv_report) = parse_report_body(i)?;
-    let (i, signature) = length_value(le_u32, |input| parse_signature(input, ak_ty))(i)?;
+    let (i, signature) = length_value(le_u32, parse_signature(ak_ty))(i)?;
     let (i, _) = eof(i)?;
     Ok((
         i,
@@ -86,27 +86,28 @@ fn parse_report_body(input: &[u8]) -> IResult<&[u8], ReportBody> {
     ))
 }
 
-fn parse_signature(input: &[u8], _attestation_key_type: u16) -> IResult<&[u8], Signature> {
-    let (i, isv_report_signature) = take(64usize)(input)?;
-    let (i, attestation_key) = take(64usize)(i)?;
-    let (i, qe_report) = parse_report_body(i)?;
-    let (i, qe_report_signature) = take(64usize)(i)?;
-    let (i, qe_authentication_data) = length_data(le_u16)(i)?;
-    let (i, qe_certification_data_type) = verify(le_u16, is_valid_cd_type)(i)?;
-    let (i, qe_certification_data) = length_value(le_u32, |input| {
-        parse_qe_cd(input, qe_certification_data_type)
-    })(i)?;
-    Ok((
-        i,
-        Signature::EcdsaP256 {
-            isv_report_signature,
-            attestation_key,
-            qe_report,
-            qe_report_signature,
-            qe_authentication_data,
-            qe_certification_data,
-        },
-    ))
+fn parse_signature(_attestation_key_type: u16) -> impl Fn(&[u8]) -> IResult<&[u8], Signature> {
+    |input: &[u8]| {
+        let (i, isv_report_signature) = take(64usize)(input)?;
+        let (i, attestation_key) = take(64usize)(i)?;
+        let (i, qe_report) = parse_report_body(i)?;
+        let (i, qe_report_signature) = take(64usize)(i)?;
+        let (i, qe_authentication_data) = length_data(le_u16)(i)?;
+        let (i, qe_certification_data_type) = verify(le_u16, is_valid_cd_type)(i)?;
+        let (i, qe_certification_data) =
+            length_value(le_u32, parse_qe_cd(qe_certification_data_type))(i)?;
+        Ok((
+            i,
+            Signature::EcdsaP256 {
+                isv_report_signature,
+                attestation_key,
+                qe_report,
+                qe_report_signature,
+                qe_authentication_data,
+                qe_certification_data,
+            },
+        ))
+    }
 }
 
 #[allow(clippy::trivially_copy_pass_by_ref)] // The macro inserts a ref and rustc won't deref it.
@@ -114,30 +115,32 @@ fn is_valid_cd_type(t: &u16) -> bool {
     *t >= 1 && *t <= 5 && *t != 4
 }
 
-fn parse_ppid_cd<'a>(
-    input: &'a [u8],
-    kind: impl FnMut(&'a [u8]) -> Ppid<'a>,
-) -> IResult<&'a [u8], QeCertificationData<'a>> {
-    let (i, ppid) = map(take(384usize), kind)(input)?;
-    let (i, cpu_svn) = take(16usize)(i)?;
-    let (i, pce_svn) = le_u16(i)?;
-    let (i, pce_id) = le_u16(i)?;
-    Ok((
-        i,
-        QeCertificationData::Ppid {
-            ppid,
-            cpu_svn,
-            pce_svn,
-            pce_id,
-        },
-    ))
+fn parse_ppid_cd<'a, F>(kind: F) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], QeCertificationData<'a>>
+where
+    F: Fn(&'a [u8]) -> Ppid<'a> + Copy,
+{
+    move |input| {
+        let (i, ppid) = map(take(384usize), kind)(input)?;
+        let (i, cpu_svn) = take(16usize)(i)?;
+        let (i, pce_svn) = le_u16(i)?;
+        let (i, pce_id) = le_u16(i)?;
+        Ok((
+            i,
+            QeCertificationData::Ppid {
+                ppid,
+                cpu_svn,
+                pce_svn,
+                pce_id,
+            },
+        ))
+    }
 }
 
-fn parse_qe_cd(i: &[u8], kind: u16) -> IResult<&[u8], QeCertificationData> {
-    match kind {
-        1 => parse_ppid_cd(i, Ppid::Clear),
-        2 => parse_ppid_cd(i, Ppid::Enc2048),
-        3 => parse_ppid_cd(i, Ppid::Enc3072),
+fn parse_qe_cd(kind: u16) -> impl Fn(&[u8]) -> IResult<&[u8], QeCertificationData> {
+    move |i| match kind {
+        1 => parse_ppid_cd(Ppid::Clear)(i),
+        2 => parse_ppid_cd(Ppid::Enc2048)(i),
+        3 => parse_ppid_cd(Ppid::Enc3072)(i),
         5 => Ok((&[], QeCertificationData::CertChain(i))),
         _ => unreachable!(),
     }
